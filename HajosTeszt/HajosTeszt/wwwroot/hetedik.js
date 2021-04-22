9cfb7e7322894ba082c5775f2501ff037e3c8ee2
corvinus-ewhn1v <aron.szabolcsi@stud.uni-corvinus.hu>
2021-04-22 11:51:49 +0200

    Egy kérdés megjelenítése REST API-n keresztül

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -90,5 +90,45 @@ function kérdésMegjelenítés(kérdés) {
             document.getElementById("válasz3").style.backgroundColor = '#FFFFFF';
         }
     
+    fetch('/questions/4')
+        .then(response => response.json())
+        .then(data => console.log(data)
+    );
 
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kérdésMegjelenítés(data)
+        );
+
+
+        console.log(kérdés);
+        document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+        document.getElementById("válasz1").innerText = kérdés.answer1
+        document.getElementById("válasz2").innerText = kérdés.answer2
+        document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+
+    function kérdésBetöltés(id) {
+        fetch(`/questions/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    console.error(`Hibás válasz: ${response.status}`)
+                }
+                else {
+                    return response.json()
+                }
+            })
+            .then(data => kérdésMegjelenítés(data));
+    }
+    function válaszfeldolgozás(válasz) {
+        if (!válasz.ok) {
+            console.error(`Hibás válasz: ${response.status}`)
+        }
+        else {
+            return válasz.json()
+        }
+    }
 }
+
+
+
